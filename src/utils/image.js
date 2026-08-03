bbb8f8c4ab664e26d93ad9e5853a1e0d9ccfae1d
yeobi <kimhyjjang@gmail.com>
2026-08-03 15:36:00 +0900

    fix: 기본 프로필이미지가 빌드 파일에 포함되지 않는 문제 수정

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,6 +1,7 @@
 import { IMAGE_BASE_URL } from "../constants/config";
+import defaultAvatar from "../../assets/default-avatar.png";
 
-const DEFAULT_AVATAR = "/assets/default-avatar.png";
+const DEFAULT_AVATAR = defaultAvatar;
 
 export function resolveImageUrl(url) {
   if (!url) return DEFAULT_AVATAR;
